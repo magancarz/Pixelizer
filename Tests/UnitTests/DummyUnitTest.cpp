d9a26deb66de4ea1525c7a34e8e71cc04a5776a0
Mateusz Gancarz <grzmateusz6@gmail.com>
2024-09-25 18:39:40 +0200

    Added job for running tests

---
+++ Tests/UnitTests/DummyUnitTest.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+TEST(OBBTests, shouldReportNoIntersectionWithSimpleOBBs)
+{
+    // given
+
+    // when
+
+    // then
+    EXPECT_FALSE(false);
+}
--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,10 +1,7 @@
 #include "gtest/gtest.h"
 
-#include <Environment.h>
-
 int main(int argc, char *argv[])
 {
-    ::testing::AddGlobalTestEnvironment(new TestsEnvironment());
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
